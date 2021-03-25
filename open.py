8f3e15f7c611745023e06563c907c9c075e57dfa
CBJlove11 <2466152968@qq.com>
2021-03-25 11:32:21 +0800

    flask框架

---
+++ open.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index_cg():
+    return render_template("采购日报.html")
+@app.route('/仓库日报.html')
+def index_ck():
+    return render_template('仓库日报.html')
+
+@app.route('/send_message', methods=['GET'])
+def send_message():
+    global message_get
+    message_get = ""
+
+    message_get = request.args['message']
+    print("收到前端发过来的信息：%s" % message_get)
+    print("收到数据的类型为：" + str(type(message_get)))
+
+    return "收到消息"
+
+
+@app.route('/change_to_json', methods=['GET'])
+def change_to_json():
+
+    global message_get
+    message_json = {
+        "message": message_get
+    }
+
+    return jsonify(message_json)
+
+
+if __name__ == '__main__':
+    app.run()
+
